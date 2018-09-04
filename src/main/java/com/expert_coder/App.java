59f166f4bd405b8c0d0ca354a17b049e9dd1798e
aditkumar1 <aak573@nyu.edu>
2018-09-04 19:39:29 -0400

    feat(master): add maven project

---
+++ src/main/java/com/expert_coder/App.java
@@ -0,0 +1,13 @@
+package com.expert_coder;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
