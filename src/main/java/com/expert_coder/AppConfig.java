9e8c6e1692b88547011be9379937ac1e9ced105c
aditkumar1 <aak573@nyu.edu>
2018-09-05 02:36:16 -0400

    feat(master): add jenkins file

---
+++ src/main/java/com/expert_coder/AppConfig.java
@@ -0,0 +1,16 @@
+package com.expert_coder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@PropertySource("application.properties")
+public class AppConfig {
+    public static void main( String[] args )
+    {
+        SpringApplication springBootApplication=new SpringApplication(AppConfig.class);
+        springBootApplication.run();
+    }
+}
